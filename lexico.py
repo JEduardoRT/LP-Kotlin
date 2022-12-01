f6a8db32ac4b5b5a79ef709140ee81a14376559a
Luis Alejandro Donoso Bravo <74844225+GhostWarrior25@users.noreply.github.com>
2022-12-01 09:27:49 -0500

    actualizacion

---
+++ lexico.py
@@ -0,0 +1,175 @@
+import ply.lex as lex
+
+#Ysrael Larco (inicio)
+
+# Tokens
+reserved = {
+  'if':'IF',
+  'for':'FOR',
+  'while':'WHILE',
+  'switch':'SWITCH',
+  'as':'AS',
+  'break':'BREAK',
+  'else':'ELSE',
+  'typealias':'TYPEALIAS',
+  'typeof':'TYPEOF',
+  'static':'STATIC',
+  'abstract':'ABSTRACT',
+  'import':'IMPORT',
+  'enum':'ENUM',
+  'in':'IN',
+  'super':'SUPER',
+  'async':'ASYNC',
+  'interface':'INTERFACE',
+  'infix':'INFIX',
+  'extends':'EXTENDS',
+  'is':'IS',
+  'sync':'SYNC',
+  'setparam':'SETPARAM',
+  'external':'EXTERNAL',
+  'library':'LIBRARY',
+  'this':'THIS',
+  'case':'CASE',
+  'factory':'FACTORY',
+  'override':'OVERRIDE',
+  'throw':'THROW',
+  'catch':'CATCH',
+  'false':'FALSE',
+  'true':'TRUE',
+  'new':'NEW',
+  'class':'CLASS',
+  'final':'FINAL',
+  'null':'NULL',
+  'try':'TRY',
+  'const':'CONST',
+  'val':'VAL',
+  'var':'VAR',
+  'open':'OPEN',
+  'continue':'CONTINUE',
+  'operator':'OPERATOR',
+  'init':'INIT',
+  'function':'FUNCTION',
+  'part':'PART',
+  'void':'VOID',
+  'default':'DEFAULT',
+  'get':'GET',
+  'constructor':'CONSTRUCTOR',
+  'when':'WHEN',
+  'hide':'HIDE',
+  'return':'RETURN',
+  'with':'WITH',
+  'do':'DO',
+  'set':'SET',
+  'out':'OUT',
+  'Int': 'TINT',
+  'Float': 'TFLOAT',
+  'String': 'STRING',
+  'println': 'PRINTLN',
+  'list': 'LIST',
+  'listOf': 'LISTOF',
+  'mutableList': 'MUTABLELIST',
+  'mutableListOf': 'MUTABLELISTOF',
+  'pair': 'PAIR',
+  'readLine': 'READLINE',
+  'fun': 'FUN'
+}
+
+#Ysrael Larco (final)
+
+#Jandry Rodriguez (inicio)
+
+tokens = [
+  'VINT',
+  'VFLOAT',
+  'MINUS',
+  'PLUS',
+  'TIMES',
+  'DIVIDE',
+  'LPAREN',
+  'RPAREN',
+  'LKEY',
+  'RKEY',
+  'IGUAL',
+  'MINOR',
+  'MAYOR',
+  'NEQUAL',
+  'AND',
+  'OR',
+  'VARIABLE',
+  'DOUBLEPOINTS',
+  'DOT',
+  'COMA',
+  'ADMIRATION',
+  'NOT'
+] + list(reserved.values())
+
+#Jandry Rodriguez (final)
+
+#Luis Donoso (inicio)
+
+# Regular expression rules for simple tokens
+t_PLUS     = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LKEY  = r'\{'
+t_RKEY  = r'\}'
+t_IGUAL = r'='
+t_MINOR = r'<'
+t_MAYOR = r'>'
+t_NEQUAL = r'<>'
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_DOUBLEPOINTS = r':'
+t_DOT = r'\.'
+t_COMA = r','
+t_NOT = r'!'
+
+#Luis Donoso (inicio)
+
+#Jandry Rodriguez (inicio)
+
+# A regular expression rule with some action code
+def t_VFLOAT(t):
+  r'\d+\.\d+'
+  t.value = float(t.value)
+  return t
+  
+def t_VINT(t):
+  r'\d+'
+  t.value = int(t.value)    
+  return t
+    
+def t_VARIABLE(t):
+  r'[a-zA-Z_][a-zA-Z0-9]*'
+  t.type = reserved.get(t.value,'VARIABLE')
+  return t
+  
+# Define a rule so we can track line numbers
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
+ 
+#Jandry Rodriguez (final)
+
+
+#Ysrael Larco (inicio)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+def t_COMMENTS(t):
+  r'\#.*'
+  pass
+  
+# Error handling rule
+def t_error(t):
+  print("Caracter no permitido'%s'" % t.value[0])
+  t.lexer.skip(1)
+ 
+ # Build the lexer
+lexer = lex.lex()
+
+
---
+++ sintactico.py
@@ -0,0 +1,290 @@
+import ply.yacc as yacc
+from lexico import tokens
+
+def p_documento(p): 
+    '''documento : implementa_funcion 
+    | instrucciones'''
+    
+def p_cuerpoF(p):
+  '''cuerpoF : asignacion
+  | impresion
+  | asignadores
+  | comparators
+  | list
+  | mutableList
+  | pair
+  | inData
+  | if
+  | for
+  | while
+  | when 
+  '''
+
+def p_asignacion(p):
+  'asignacion : designacion VARIABLE DOUBLEPOINTS tipo IGUAL valor'
+
+#Donoso Bravo Luis Alejandro
+def p_designacion(p):
+    """designacion : VAL 
+        | VAR
+    """
+
+#Donoso Bravo Luis Alejandro
+
+def p_tipo(p):
+    """tipo : TINT
+        | TFLOAT
+        | STRING
+    """
+
+#Donoso Bravo Luis Alejandro
+
+def p_diamondtype(p):
+    """diamondType : MINOR tipo MAYOR"""
+#Donoso Bravo Luis Alejandro
+
+def p_listAsignacion(p):
+    ''' listAsignacion : designacion  VARIABLE DOUBLEPOINTS LIST diamondType
+    | designacion VARIABLE
+    '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_listOf(p):
+    '''listOf : LISTOF LPAREN elementos RPAREN '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_elementos(p):
+    '''elementos : elementos COMA valor
+    | valor
+    '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_valor(p):
+    '''valor : VINT
+    | VFLOAT
+    | STRING
+    '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_list(p):
+    '''list : listAsignacion IGUAL listOf
+    '''
+
+#ysrael larco faubla
+def p_add(p):
+  '''add : operado PLUS operado
+  '''
+#ysrael larco faubla
+def p_subtract(p):
+  '''subtract : operado MINUS operado
+  '''
+#ysrael larco faubla
+def p_multiply(p):
+  '''multiply : operado TIMES operado
+  '''
+  #ysrael larco faubla
+def p_divide(p):
+  '''divide : operado DIVIDE operado
+  '''
+
+def p_operado(p):
+    '''operado : add
+    | subtract
+    | multiply
+    | divide
+    | dato
+    '''
+#Donoso Bravo Luis Alejandro
+
+def p_mutListAsignacion(p):
+    ''' mutListAsignacion : designacion VARIABLE DOUBLEPOINTS MUTABLELIST diamondType
+    | designacion VARIABLE
+    '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_mutableListOf(p):
+    '''mutableListOf : MUTABLELISTOF LPAREN elementos RPAREN'''
+
+#Donoso Bravo Luis Alejandro
+
+def p_mutableList(p):
+    '''mutableList : mutListAsignacion IGUAL mutableListOf
+    '''
+
+#Donoso Bravo Luis Alejandro
+
+def p_pairAsignation(p):
+    '''pairAsignation : designacion LPAREN VARIABLE COMA VARIABLE RPAREN'''
+
+#Donoso Bravo Luis Alejandro
+
+def p_pairBody(p):
+    '''pairBody : PAIR LPAREN valor COMA valor RPAREN'''
+
+#Donoso Bravo Luis Alejandro
+
+def p_pair(p):
+    '''pair : pairAsignation IGUAL pairBody
+    '''
+#Donoso Bravo Luis Alejandro
+
+def p_inData(p):
+    '''inData : designacion VARIABLE IGUAL READLINE LPAREN RPAREN'''
+
+#ysrael larco faubla
+def p_compareType(p):
+    '''compareType : dato IGUAL IGUAL dato
+    '''
+
+def p_compareNotEqual(p):
+    '''compareNotEqual : dato NOT IGUAL dato
+    '''
+
+def p_comparators(p):
+    '''comparators : compareType
+    | compareGreaterthan
+    | compareSmallerthan
+    | compareNotEqual
+    '''
+#ysrael larco faubla
+def p_compareGreaterthan(p):
+    '''compareGreaterthan : dato MAYOR dato
+  '''
+ #ysrael larco faubla 
+def p_compareSmallerthan(p):
+    '''compareSmallerthan : dato MINOR dato
+  '''
+#ysrael larco faubla
+def p_impresion(p):
+  'impresion : PRINTLN LPAREN datoretornado RPAREN'
+
+def p_datoretornado(p):
+    '''datoretornado : operado
+    | booly
+    '''
+def p_condicion(p):
+    ''' condicion : booly
+    '''
+
+def p_booly(p):
+    '''booly : comparators
+    | logic
+    | TRUE
+    | FALSE'''
+
+def  p_logic(p):
+    '''logic : booly logicoperador booly
+    | NOT booly
+    '''
+
+def p_logicoperador(p):
+    '''logicoperador : AND
+    | OR'''
+
+#ysrael larco faubla
+def p_if(p):
+    '''if : IF LPAREN condicion RPAREN LKEY instrucciones RKEY
+    | IF LPAREN condicion RPAREN LKEY instrucciones RKEY else
+    | IF LPAREN condicion RPAREN LKEY instrucciones RKEY elseif else
+    '''
+
+def p_elseif(p):
+    'elseif : ELSE IF LPAREN condicion RPAREN LKEY instrucciones RKEY'
+
+def p_else(p):
+    'else : ELSE LKEY instrucciones RKEY'
+
+def p_condicionFor(p):
+    ''' condicionFor : condicionRango
+    | condicionBloque
+    '''
+
+def p_condicionRango(p):
+    'condicionRango :  VARIABLE IN VINT DOT DOT VINT '
+
+def p_condicionBloque(p):
+    'condicionBloque : VARIABLE IN LISTOF'
+
+#ysrael larco faubla
+def p_for(p):
+    'for : FOR LPAREN condicionFor RPAREN LKEY instrucciones RKEY'
+
+#ysrael larco faubla
+def p_while(p):
+    'while : WHILE LPAREN condicion RPAREN LKEY instrucciones RKEY'
+
+#ysrael larco faubla
+def p_when(p):
+    'when : WHEN LPAREN VARIABLE RPAREN LKEY instrucciones RKEY'
+
+def p_implementa_funcion(p):
+
+    '''implementa_funcion : FUN VARIABLE LPAREN listaparametros RPAREN DOUBLEPOINTS tipo LKEY instrucciones retorno RKEY
+    | FUN VARIABLE LPAREN RPAREN DOUBLEPOINTS tipo LKEY instrucciones retorno RKEY
+    | FUN VARIABLE LPAREN RPAREN DOUBLEPOINTS VOID LKEY instrucciones RKEY
+    | FUN VARIABLE LPAREN listaparametros RPAREN DOUBLEPOINTS VOID LKEY instrucciones RKEY'''
+
+def p_listaparametros(p):
+    '''listaparametros : parametro
+    | parametro COMA listaparametros'''
+
+def p_parametro(p):
+    'parametro : VARIABLE DOUBLEPOINTS tipo'
+
+def p_retorno(p):
+  'retorno : RETURN asignado'
+
+def p_instrucciones(p):
+  '''instrucciones : cuerpoF
+      | cuerpoF instrucciones'''
+
+def p_asignadores(p):
+    '''asignadores : asigna
+    | plusasigna
+    | minasigna
+    | multasigna
+    | divasigna'''
+
+def p_asigna(p):
+  'asigna : VARIABLE IGUAL asignado'
+
+def p_plusasigna(p):
+    'plusasigna : VARIABLE PLUS IGUAL asignado'
+
+def p_minasigna(p):
+    'minasigna : VARIABLE MINUS IGUAL asignado'
+
+def p_multasigna(p):
+    'multasigna : VARIABLE TIMES IGUAL asignado'
+
+def p_divasigna(p):
+    'divasigna : VARIABLE DIVIDE IGUAL asignado'
+
+def p_asignado(p):
+  '''asignado : comparators
+| operado'''
+
+def p_dato(p):
+    '''dato : valor
+    | VARIABLE
+    '''
+
+def p_error(p):
+    global output
+    if p:
+        output += 'Error de sintaxis - Token: '+ str(p.type)+', Línea: '+str(p.lineno)+', Col: '+str(p.lexpos)+'\n'
+        parser.errok()
+    else:
+        output += 'Error de sintaxis Fin de Linea\n'
+
+
+# Build the parser
+parser = yacc.yacc()
+
+
+output = ''
